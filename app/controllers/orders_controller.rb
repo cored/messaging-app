e7abe8af5135191019747a0e0edfeaea4d2681b1
Rafael George <george.rafael@gmail.com>
2024-12-13 10:42:38 -0700

    fix: add missing new action for orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,10 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_order, only: %i[ show edit update destroy ]
 
+  def new
+    @order = Order.new
+  end
+
   # GET /orders or /orders.json
   def index
     @orders = current_user.is_doctor ? current_user.doctor_orders : current_user.orders
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
+    @user = users(:one)
+    sign_in @user
     @order = orders(:one)
   end
 
