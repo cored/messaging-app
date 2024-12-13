666b4f4da8c883cf8b4945dda062715c644ed763
Rafael George <george.rafael@gmail.com>
2024-12-13 11:41:02 -0700

    feat: add migration for new roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  role                   :integer          default(0), not null
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
@@ -27,6 +28,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :orders
   has_many :messages
+
+  enum role: { user: 0, doctor: 1, customer_care: 2 }
 
   def doctor_orders
     Order.where(doctor_id: self.id)
